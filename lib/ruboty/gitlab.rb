0f9aaad5a84868785869c56e1f768869d97089e3
fakestarbaby <fakestarbaby@gmail.com>
2016-01-13 21:11:01 +0900

    Add create gitlab issue command

--- lib/ruboty/gitlab.rb
+++ lib/ruboty/gitlab.rb
@@ -1,5 +1,9 @@
+require "active_support/core_ext/object/blank"
+require "gitlab"
+
 require "ruboty"
 require "ruboty/gitlab/actions/base"
+require "ruboty/gitlab/actions/create_issue"
 require "ruboty/gitlab/actions/remember"
 require "ruboty/gitlab/version"
 require "ruboty/handlers/gitlab"
--- lib/ruboty/gitlab/actions/base.rb
+++ lib/ruboty/gitlab/actions/base.rb
@@ -16,8 +16,59 @@ module Ruboty
           message.robot.brain.data[NAMESPACE] ||= {}
         end
 
+        def require_private_token
+          message.reply("I don't know your gitlab private token")
+        end
+
+        def has_private_token?
+          !!private_token
+        end
+
+        def private_token
+          @private_token ||= private_tokens[sender_name]
+        end
+
         def sender_name
           message.from_name
+        end
+
+        def projects
+          message.robot.brain.data[NAMESPACE][:projects] ||= {}
+        end
+
+        def exists_project?
+          project.present?
+        end
+
+        def search_project
+          client.search_projects(given_project).first
+        end
+
+        def project
+          projects[given_project] ||= search_project
+        end
+
+        def given_project
+          message[:project]
+        end
+
+        def client
+          ::Gitlab::Client.new(client_options)
+        end
+
+        def client_options
+          client_options_with_nil_value.reject {|key, value| value.nil? }
+        end
+
+        def client_options_with_nil_value
+          {
+            endpoint: gitlab_endpoint_url,
+            private_token: private_token,
+          }
+        end
+
+        def gitlab_endpoint_url
+          ENV["GITLAB_URL"] + "/api/v3"
         end
       end
     end
